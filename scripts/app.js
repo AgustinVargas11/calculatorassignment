2da2f332aeb7b4bfd30eed35d7fe3cfbab9bbca9
Agustin Vargas <agustinvargas11@gmail.com>
2016-08-19 16:32:58 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// collect inputs from input elements, add them then write value to the dom
+function add() {
+    var number1 = document.getElementById('add1').value,
+        number2 = document.getElementById('add2').value,
+        result  = document.getElementById('addition-screen');
+    
+    result.innerHTML = Number(number1) + Number(number2);
+}
+
+
+// collect inputs from input elements, subtract them then write value to the dom
+function subtract() {
+    var number1 = document.getElementById('sub1').value,
+        number2 = document.getElementById('sub2').value,
+        result  = document.getElementById('subtraction-screen');
+    
+    result.innerHTML = Number(number1) - Number(number2);
+}
+
+
+// collect inputs from input elements, multiply them then write value to the dom
+function multiply() {
+    var number1 = document.getElementById('mult1').value,
+        number2 = document.getElementById('mult2').value,
+        result  = document.getElementById('multiplication-screen');
+    
+    result.innerHTML = Number(number1) * Number(number2);
+}
+
+
+var expanded = false;
+// expand input boxes or shrink if already expanded
+function expandInputs() {
+    if (!expanded) {
+        expanded = true;
+        this.parentNode.children[2].classList.add('full-width-toggle');
+        this.parentNode.children[3].classList.add('full-width-toggle');
+    } else {
+        this.parentNode.children[2].classList.remove('full-width-toggle');
+        this.parentNode.children[3].classList.remove('full-width-toggle');
+        expanded = false;
+    }
+    
+}
+
+// add event listeners
+var addButton = document.getElementById('addition');
+addButton.addEventListener('click', add);
+
+var subButton = document.getElementById('minus');
+subButton.addEventListener('click', subtract);
+
+var multButton = document.getElementById('times');
+multButton.addEventListener('click', multiply);
+
+
+var toggles = document.getElementsByClassName('input-toggle');
+
+// add even listener to all elements in the toggles array
+for (var i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('click', expandInputs);
+}
+
+
+
